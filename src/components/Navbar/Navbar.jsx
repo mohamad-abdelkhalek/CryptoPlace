c0066c3acfec287218be764f6e83ab0a2fffd9a8
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-12-05 18:37:26 +0200

    Added Navbar HTML structure

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../assets/logo.png'
+import arrow_icon from '../../assets/arrow_icon.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+        <img src={logo} alt="logo" />
+        <ul>
+            <li>Home</li>
+            <li>Features</li>
+            <li>Pricing</li>
+            <li>Blog</li>
+        </ul>
+        <div className="nav-right">
+            <select>
+                <option value="usd">USD</option>
+                <option value="eur">EUR</option>
+            </select>
+            <button>Sign up <img src={arrow_icon} alt="arrow" />
+            </button>
+        </div>
+    </div>
   )
 }
 
