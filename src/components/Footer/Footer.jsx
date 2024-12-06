4702cd8ac87c29522a7b27c3ac1430e25b3a9307
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-12-06 20:44:34 +0200

    Added Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div>
+        <p>Copright © 2024, Cryptoplace - All Rights Reserved</p>
+    </div>
+  )
+}
+
+export default Footer
