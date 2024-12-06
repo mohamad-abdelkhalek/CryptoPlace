a8707560a67733eb6bf136bd4eb82eed47d12f99
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-12-06 19:33:48 +0200

    Added fetching all coins data using API function

--- src/context/CoinContext.jsx
+++ src/context/CoinContext.jsx
@@ -4,11 +4,24 @@ export const CoinContext = createContext();
 
 const CoinContextProvider = (props) => {
 
-    const [allCoin, setAllCoin] = useState([]);
+    const [allCoins, setAllCoins] = useState([]);
     const [currency, setCurrency] = useState({
         name: "usd",
         symbol: "$"
     });
+
+    const fetchAllCoins = async () => {
+        const options = {
+            method: 'GET',
+            headers: {accept: 'application/json',
+                     'x-cg-demo-api-key': 'CG-BB7K3SpjGBWPABagbnsMwbqX'}
+          };
+          
+          fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd', options)
+            .then(res => res.json())
+            .then(res => setAllCoins(res))
+            .catch(err => console.error(err));
+    }
 
     const contextValue = {
 
