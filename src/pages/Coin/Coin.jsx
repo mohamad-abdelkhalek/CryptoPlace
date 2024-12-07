716a59fb7a145ac6dc17c7645cda79dedfb2feb0
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-12-07 19:51:24 +0200

    Fixed error in getting image & added error handling

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -1,65 +1,87 @@
-import React, { useContext, useEffect, useState } from 'react'
-import './Coin.css'
-import { useParams } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from 'react';
+import './Coin.css';
+import { useParams } from 'react-router-dom';
 import { CoinContext } from '../../context/CoinContext';
 import LineChart from '../../components/LineChart/LineChart';
 
 const Coin = () => {
-
-  const {coinId} = useParams();
-  const [coinData, setCoinData] = useState();
-  const [historicalData, setHistoricalData] = useState();
-  const {currency} = useContext(CoinContext);
+  const { coinId } = useParams();
+  const [coinData, setCoinData] = useState(null);
+  const [historicalData, setHistoricalData] = useState(null);
+  const { currency } = useContext(CoinContext);
 
   const fetchCoinData = async () => {
     const options = {
       method: 'GET',
-      headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-BB7K3SpjGBWPABagbnsMwbqX'}
+      headers: {
+        accept: 'application/json',
+        'x-cg-demo-api-key': 'CG-BB7K3SpjGBWPABagbnsMwbqX',
+      },
     };
-    
-    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`, options)
-      .then(res => res.json())
-      .then(res => setCoinData(res))
-      .catch(err => console.error(err));
-  }
+
+    try {
+      const response = await fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`, options);
+      const data = await response.json();
+      setCoinData(data);
+    } catch (err) {
+      console.error('Error fetching coin data:', err);
+    }
+  };
 
   const fetchHistoricalData = async () => {
     const options = {
       method: 'GET',
-      headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-BB7K3SpjGBWPABagbnsMwbqX'}
+      headers: {
+        accept: 'application/json',
+        'x-cg-demo-api-key': 'CG-BB7K3SpjGBWPABagbnsMwbqX',
+      },
     };
-    
-    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=10&interval=daily`, options)
-      .then(res => res.json())
-      .then(res => setHistoricalData(res))
-      .catch(err => console.error(err));
-  }
+
+    try {
+      const response = await fetch(
+        `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=10&interval=daily`,
+        options
+      );
+      const data = await response.json();
+      setHistoricalData(data);
+    } catch (err) {
+      console.error('Error fetching historical data:', err);
+    }
+  };
 
   useEffect(() => {
     fetchCoinData();
     fetchHistoricalData();
-  }, [currency])
+  }, [currency]);
 
-if(coinData, historicalData){
+  // Loading state
+  if (!coinData || !historicalData) {
+    return (
+      <div className="spinner">
+        <div className="spin"></div>
+      </div>
+    );
+  }
+
+  // Safeguard against missing properties in `coinData`
+  const { image, name, symbol } = coinData || {};
+  const coinImage = image?.large;
+
   return (
-    <div className='coin'>
+    <div className="coin">
       <div className="coin-name">
-        <img src={coinData.image.large} alt="Coin Image" />
-        <p><b>{coinData.name} ({coinData.symbol.toUpperCase()})</b></p>
+        {coinImage ? <img src={coinImage} alt={`${name} Image`} /> : <p>No Image Available</p>}
+        <p>
+          <b>
+            {name} ({symbol?.toUpperCase()})
+          </b>
+        </p>
       </div>
       <div className="coin-chart">
         <LineChart historicalData={historicalData} />
       </div>
     </div>
-  )
-}else{
-  return (
-    <div className='spinner'>
-      <div className="spin"></div>
-    </div>
-  )
-}
-  
-}
+  );
+};
 
-export default Coin
+export default Coin;
