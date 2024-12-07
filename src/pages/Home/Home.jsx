e2d3dc0a24317bc53a60ab693933b4cab6bec15b
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-12-07 17:01:23 +0200

    Updated linking to coin details page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import './Home.css'
 import { CoinContext } from '../../context/CoinContext';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 
@@ -56,7 +57,7 @@ const Home = () => {
         </div>
         {
           displayCoin.slice(0, 10).map((item, index) => (
-            <div className="table-layout" key={index}>
+            <Link to={`/coin/${item.id}`} className="table-layout" key={index}>
               <p>{item.market_cap_rank}</p>
               <div>
                 <img src={item.image} alt="Coin image" />
@@ -67,7 +68,7 @@ const Home = () => {
                 {Math.floor(item.price_change_percentage_24h*100)/100}
               </p>
               <p className='market-cap'>{currency.symbol} {item.market_cap.toLocaleString()}</p>
-            </div>
+            </Link>
           ))
         }
 
