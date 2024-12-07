27b948f7310f59b324786d873a639f205d41dd8f
Mohammad Abdelkhalek <138255130+mohamad-abdelkhalek@users.noreply.github.com>
2024-12-07 19:16:28 +0200

    Added fetching for historical data of the coin function

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -7,6 +7,7 @@ const Coin = () => {
 
   const {coinId} = useParams();
   const [coinData, setCoinData] = useState();
+  const [historicalData, setHistoricalData] = useState();
   const {currency} = useContext(CoinContext);
 
   const fetchCoinData = async () => {
@@ -18,6 +19,18 @@ const Coin = () => {
     fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`, options)
       .then(res => res.json())
       .then(res => setCoinData(res))
+      .catch(err => console.error(err));
+  }
+
+  const fetchHistoricalData = async () => {
+    const options = {
+      method: 'GET',
+      headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-BB7K3SpjGBWPABagbnsMwbqX'}
+    };
+    
+    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=10`, options)
+      .then(res => res.json())
+      .then(res => setHistoricalData(res))
       .catch(err => console.error(err));
   }
 
